d51a09e3b1b1d11330eda3669c0ba7d88b37abba
Josue Alexander Ibarra <1480657+ibarrajo@users.noreply.github.com>
2025-01-09 11:12:37 -0800

    Update dependencies (#209)
    * Update 05-child-workflows.md
    
    * throw error on markdown broken link
    
    * gh action remove test job
    
    * update dependencies
    
    ---------
    
    Co-authored-by: Wissem Belguidoum <5852356+elbombardi@users.noreply.github.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
