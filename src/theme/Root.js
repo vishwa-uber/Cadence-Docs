be14893d8848869861ebebe9d693abca92b1ef33
Vishwa Patil <vpatil16@ext.uber.com>
2025-09-30 13:07:56 -0700

    Pop-up Needs to Go (#286)

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import GrafanaSetupPopup from '../components/GrafanaSetupPopup';
 
 export default function Root({ children }) {
-  console.log('🎯 Root component loaded - single popup system initialized');
-  
   return (
     <>
       {children}
-      <GrafanaSetupPopup />
     </>
   );
 }
